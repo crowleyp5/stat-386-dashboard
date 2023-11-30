fa36164e25115295600d5997fdb0f1645433ebcc
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-11-29 21:08:26 -0700

    Update lab-10-crowleyp5.py

--- lab-10-crowleyp5.py
+++ lab-10-crowleyp5.py
@@ -2,16 +2,14 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 data_url = "https://raw.githubusercontent.com/esnt/Data/main/Names/popular_names.csv"
 data = pd.read_csv(data_url)
 
-st.title("Baby Names Analysis")
+st.title("Popular Names")
 st.header("Top Baby Names Analysis")
 
 selected_year = st.slider("Select a year:", min_value=1910, max_value=2021, value=1910, step=1)
 data_selected_year = data[data['year'] == selected_year]
-
 top_names_selected_year = (
     data_selected_year.groupby(['name', 'sex'])['n']
     .sum()
@@ -21,11 +19,8 @@ top_names_selected_year = (
     .head(10)
     .sort_values(by='n', ascending=False)
 )
-
-fig, axes = plt.subplots(3, 1, figsize=(18, 18), gridspec_kw={'height_ratios': [5, 5, 6]})
-
+fig, axes = plt.subplots(3, 1, figsize=(12, 16), gridspec_kw={'height_ratios': [5, 5, 5]})
 palette = sns.color_palette("husl")
-
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'F'],
     x='name',
@@ -36,7 +31,6 @@ sns.barplot(
 axes[0].set_title(f'Top Baby Names for Females in {selected_year}')
 axes[0].set_xlabel('Name')
 axes[0].set_ylabel('Count (n)')
-
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'M'],
     x='name',
@@ -46,14 +40,12 @@ sns.barplot(
 )
 axes[1].set_title(f'Top Baby Names for Males in {selected_year}')
 axes[1].set_xlabel('Name')
-axes[1].set_ylabel('Count (n)')
-
+axes[0].tick_params(axis='x', rotation=45)
+axes[1].tick_params(axis='x', rotation=45)
 selected_name = st.text_input(f"Enter a name to see popularity over time in {selected_year}:")
-
 if selected_name:
-    name_data = data[(data['name'] == selected_name) & (data['year'] >= 1910) & (data['year'] <= 2021)]
+    name_data = data[(data['name'] == selected_name) & (data['year'] >= selected_year - 10) & (data['year'] <= selected_year + 10)]
     if not name_data.empty:
-        # Create a separate plot for popularity over time
         sns.lineplot(
             data=name_data,
             x='year',
@@ -65,13 +57,11 @@ if selected_name:
         axes[2].set_title(f'Popularity of {selected_name} Over Time')
         axes[2].set_xlabel('Year')
         axes[2].set_ylabel('Count (n)')
-        # Set fixed x-axis limits
         axes[2].set_xlim(1910, 2021)
     else:
         st.warning(f"Name '{selected_name}' not found in the dataset.")
 else:
-    axes[2].set_visible(False)
-
+    st.info("Enter a name to see popularity over time.")
 plt.tight_layout()
-
+st.pyplot(fig)
 st.text("Explore the top baby names and their popularity over time.")
