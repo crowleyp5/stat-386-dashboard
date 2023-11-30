7012903a5f380c254ef0932e4d0e215e984a3557
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-11-29 20:44:45 -0700

    Update lab-10-crowleyp5.py

--- lab-10-crowleyp5.py
+++ lab-10-crowleyp5.py
@@ -3,19 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load data from the dataset URL
 data_url = "https://raw.githubusercontent.com/esnt/Data/main/Names/popular_names.csv"
 data = pd.read_csv(data_url)
 
-# Streamlit app title and description
 st.title("Baby Names Analysis")
 st.header("Top Baby Names Analysis")
 
-# Filter data by year
-selected_year = st.slider("Select a year:", min_value=1880, max_value=2020, value=1910, step=1)
+selected_year = st.slider("Select a year:", min_value=1910, max_value=2021, value=1910, step=1)
 data_selected_year = data[data['year'] == selected_year]
 
-# Calculate the top 10 baby names by 'n' for both genders
 top_names_selected_year = (
     data_selected_year.groupby(['name', 'sex'])['n']
     .sum()
@@ -26,13 +22,10 @@ top_names_selected_year = (
     .sort_values(by='n', ascending=False)
 )
 
-# Create subplots for males, females, and popularity over time
-fig, axes = plt.subplots(1, 3, figsize=(18, 6), gridspec_kw={'width_ratios': [1, 1, 2]})  # 3 subplots side by side
+fig, axes = plt.subplots(3, 1, figsize=(18, 18), gridspec_kw={'width_ratios': [1, 1, 2]})
 
-# Define color palette
 palette = sns.color_palette("husl")
 
-# Create bar chart for top baby names for females
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'F'],
     x='name',
@@ -44,7 +37,6 @@ axes[0].set_title(f'Top Baby Names for Females in {selected_year}')
 axes[0].set_xlabel('Name')
 axes[0].set_ylabel('Count (n)')
 
-# Create bar chart for top baby names for males
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'M'],
     x='name',
@@ -55,14 +47,11 @@ sns.barplot(
 axes[1].set_title(f'Top Baby Names for Males in {selected_year}')
 axes[1].set_xlabel('Name')
 
-# Rotate x-axis labels for better readability
 axes[0].tick_params(axis='x', rotation=45)
 axes[1].tick_params(axis='x', rotation=45)
 
-# User input for the name
 selected_name = st.text_input(f"Enter a name to see popularity over time in {selected_year}:")
 
-# Filter data for the selected name and plot popularity over time
 if selected_name:
     name_data = data[(data['name'] == selected_name) & (data['year'] >= selected_year - 10) & (data['year'] <= selected_year + 10)]
     if not name_data.empty:
@@ -82,11 +71,8 @@ if selected_name:
 else:
     st.info("Enter a name to see popularity over time.")
 
-# Adjust spacing between subplots
 plt.tight_layout()
 
-# Display the Seaborn subplots within Streamlit
 st.pyplot(fig)
 
-# Streamlit app description
 st.text("Explore the top baby names and their popularity over time.")
