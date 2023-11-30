c2085e95a2dd46d90265f327d7ddca2f7000265c
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-11-29 19:26:54 -0700

    Update lab-10-crowleyp5.py

--- lab-10-crowleyp5.py
+++ lab-10-crowleyp5.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data from the new dataset URL
 data_url = "https://raw.githubusercontent.com/esnt/Data/main/Names/popular_names.csv"
@@ -9,13 +10,27 @@ data = pd.read_csv(data_url)
 st.title('Popular Names Analysis')
 
 # Sidebar with user input
-selected_columns = st.sidebar.multiselect('Select columns for the bar chart:', data.columns)
+selected_columns = st.sidebar.multiselect('Select columns:', data.columns)
 
-# Create a bar chart based on user selection
-if selected_columns:
+# Check if 'year', 'name', and 'n' are selected
+if 'year' in selected_columns and 'name' in selected_columns and 'n' in selected_columns:
     st.write('Selected columns:', selected_columns)
-    selected_data = data[selected_columns]
-    st.bar_chart(selected_data)
+    
+    # Group data by year and name, summing the counts 'n'
+    grouped_data = data.groupby(['year', 'name'])['n'].sum().reset_index()
+
+    # Create a bar chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+    for name, group in grouped_data.groupby('name'):
+        ax.plot(group['year'], group['n'], label=name)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Count')
+    ax.set_title('Name Popularity Over Time')
+    ax.legend()
+
+    # Display the chart
+    st.pyplot(fig)
 
 # Main content
 st.write('Here is some data:')
