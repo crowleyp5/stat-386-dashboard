4aaf32801f1d9d4064f0e4a68f52b0e833d132e5
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-11-29 20:36:33 -0700

    Update lab-10-crowleyp5.py

--- lab-10-crowleyp5.py
+++ lab-10-crowleyp5.py
@@ -3,15 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load data from the dataset URL
 data_url = "https://raw.githubusercontent.com/esnt/Data/main/Names/popular_names.csv"
 data = pd.read_csv(data_url)
 
+# Streamlit app title and description
 st.title("Baby Names Analysis")
 st.header("Top Baby Names Analysis")
 
+# Filter data by year
 selected_year = st.slider("Select a year:", min_value=1910, max_value=2021, value=1910, step=1)
 data_selected_year = data[data['year'] == selected_year]
 
+# Calculate the top 10 baby names by 'n' for both genders
 top_names_selected_year = (
     data_selected_year.groupby(['name', 'sex'])['n']
     .sum()
@@ -22,10 +26,13 @@ top_names_selected_year = (
     .sort_values(by='n', ascending=False)
 )
 
-fig, axes = plt.subplots(1, 2, figsize=(18, 9))  # 2 subplots side by side with doubled height
+# Create a subplot with three plots
+fig, axes = plt.subplots(3, 1, figsize=(18, 18))  # 3 subplots stacked vertically with increased height
 
+# Define color palette
 palette = sns.color_palette("husl")
 
+# Create bar chart for top baby names for females
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'F'],
     x='name',
@@ -37,6 +44,7 @@ axes[0].set_title(f'Top Baby Names for Females in {selected_year}')
 axes[0].set_xlabel('Name')
 axes[0].set_ylabel('Count (n)')
 
+# Create bar chart for top baby names for males
 sns.barplot(
     data=top_names_selected_year[top_names_selected_year['sex'] == 'M'],
     x='name',
@@ -46,34 +54,32 @@ sns.barplot(
 )
 axes[1].set_title(f'Top Baby Names for Males in {selected_year}')
 axes[1].set_xlabel('Name')
+axes[1].set_ylabel('Count (n)')
 
-axes[0].tick_params(axis='x', rotation=45)
-axes[1].tick_params(axis='x', rotation=45)
-
+# User input for the name
 selected_name = st.text_input(f"Enter a name to see popularity over time in {selected_year}:")
 
+# Filter data for the selected name
 if selected_name:
     name_data = data[(data['name'] == selected_name) & (data['year'] >= selected_year - 10) & (data['year'] <= selected_year + 10)]
     if not name_data.empty:
         # Create a separate plot for popularity over time
-        popularity_fig, popularity_ax = plt.subplots(figsize=(18, 9))  # Double the size for the popularity over time plot
         sns.lineplot(
             data=name_data,
             x='year',
             y='n',
             hue='sex',
             palette=palette[:2],  # Use the first two colors for females and males
-            ax=popularity_ax
+            ax=axes[2]
         )
-        popularity_ax.set_title(f'Popularity of {selected_name} Over Time')
-        popularity_ax.set_xlabel('Year')
-        popularity_ax.set_ylabel('Count (n)')
-        st.pyplot(popularity_fig)  # Pass the figure object to st.pyplot()
-    else:
-        st.warning(f"Name '{selected_name}' not found in the dataset.")
+        axes[2].set_title(f'Popularity of {selected_name} Over Time')
+        axes[2].set_xlabel('Year')
+        axes[2].set_ylabel('Count (n)')
 else:
-    st.info("Enter a name to see popularity over time.")
+    axes[2].set_visible(False)
 
+# Adjust spacing between subplots
+plt.tight_layout()
+
+# Streamlit app description
 st.text("Explore the top baby names and their popularity over time.")
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
