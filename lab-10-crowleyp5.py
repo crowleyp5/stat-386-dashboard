186747ee52786d75644cc61bae0dce4e338ac739
Paul Crowley <108026566+crowleyp5@users.noreply.github.com>
2023-11-29 20:48:41 -0700

    Update lab-10-crowleyp5.py

--- lab-10-crowleyp5.py
+++ lab-10-crowleyp5.py
@@ -22,7 +22,7 @@ top_names_selected_year = (
     .sort_values(by='n', ascending=False)
 )
 
-fig, axes = plt.subplots(3, 1, figsize=(18, 18), gridspec_kw={'height_ratios': [2, 2, 5]})
+fig, axes = plt.subplots(3, 1, figsize=(18, 18), gridspec_kw={'height_ratios': [5, 5, 5]})
 
 palette = sns.color_palette("husl")
 
